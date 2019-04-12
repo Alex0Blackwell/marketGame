52c25e6557a0663cbd3833bfbd57b7212ccd424a
Alex Blackwell <31634087+Alex0Blackwell@users.noreply.github.com>
2019-04-12 11:52:38 -0700

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ mSlotGenBig();
 
 function marketTime() {
   var t0 = 15000;
-  var t1 = 75000;
+  var t1 = 2000;
   var rawTime0 = t0 / 1000;
   var rawTime1 = t1 / 1000;
   var mTime = setInterval(mSlotGen, t0);
@@ -52,111 +52,103 @@ function mSlotGenBig() {
   var mBigItems = ['Electronics Store', 'Computer Store', 'Café', 'Restaurant'];
   var mBRand = Math.random() * 10;
 
+  genLogic();
 
-  if (mBRand < 4) { //electronics store
-    if (lastPick === mBigItems[0]) {
-      mSlotGenBig();
-    } else {
-      var bigPrice0 = mBigPriceGen(0);
-      document.getElementById('bMSlot0').innerHTML = `${mBigItems[0]} for $${bigPrice0}`;
-      document.getElementById('bMSlot0').value = `1~Electronics Store~${bigPrice0}`;
+  function genLogic() {
+    if (mBRand < 4) { //electronics store
+      if (lastPick === mBigItems[0]) {
+	    mBRand = Math.random() * 10;
+	    lastPick = mBigItems[0];
+        genLogic();
+      } else {
+        var bigPrice0 = mBigPriceGen(0);
+        document.getElementById('bMSlot0').innerHTML = `${mBigItems[0]} for $${bigPrice0}`;
+        document.getElementById('bMSlot0').value = `1~Electronics Store~${bigPrice0}`;
+	    lastPick = mBigItems[0];
+      }
+    } else if (mBRand < 7) { //computer store
+      if (lastPick === mBigItems[1]) {
+        mBRand = Math.random() * 10;
+	    lastPick = mBigItems[1];
+        genLogic();
+      } else {
+        var bigPrice1 = mBigPriceGen(1);
+        document.getElementById('bMSlot0').innerHTML = `${mBigItems[1]} for $${bigPrice1}`;
+        document.getElementById('bMSlot0').value = `1~Computer Store~${bigPrice1}`;
+	    lastPick = mBigItems[1];
+      }
+    } else if (mBRand < 9) { //cafe
+      if (lastPick === mBigItems[2]) {
+        mBRand = Math.random() * 10;
+	    lastPick = mBigItems[2];
+        genLogic();
+      } else {
+        var bigPrice2 = mBigPriceGen(2);
+        document.getElementById('bMSlot0').innerHTML = `${mBigItems[2]} for $${bigPrice2}`;
+        document.getElementById('bMSlot0').value = `1~Cafe~${bigPrice2}`;
+	    lastPick = mBigItems[2];
+      }
+    } else if (mBRand >= 9) { //restaurant
+      if (lastPick === mBigItems[3]) {
+        mBRand = Math.random() * 10;
+	    lastPick = mBigItems[3];
+        genLogic();
+     } else {
+        var bigPrice3 = mBigPriceGen(3);
+        document.getElementById('bMSlot0').innerHTML = `${mBigItems[3]} for $${bigPrice3}`;
+        document.getElementById('bMSlot0').value = `1~Restaurant~${bigPrice3}`;
+	    lastPick = mBigItems[3];
+      }
     }
-    lastPick = mBigItems[0];
-  } else if (mBRand >= 4 && mBRand < 7) { //computer store
-    if (lastPick === mBigItems[1]) {
-      mSlotGenBig();
-    } else {
-      var bigPrice1 = mBigPriceGen(1);
-      document.getElementById('bMSlot0').innerHTML = `${mBigItems[1]} for $${bigPrice1}`;
-      document.getElementById('bMSlot0').value = `1~Computer Store~${bigPrice1}`;
-    }
-    lastPick = mBigItems[1];
-  } else if (mBRand >= 7 && mBRand < 9) { //cafe
-    if (lastPick === mBigItems[2]) {
-      mSlotGenBig();
-    } else {
-      var bigPrice2 = mBigPriceGen(2);
-      document.getElementById('bMSlot0').innerHTML = `${mBigItems[2]} for $${bigPrice2}`;
-      document.getElementById('bMSlot0').value = `1~Cafe~${bigPrice2}`;
-    }
-    lastPick = mBigItems[2];
-  } else if (mBRand >= 9) { //restaurant
-    if (lastPick === mBigItems[3]) {
-      mSlotGenBig();
-    } else {
-      var bigPrice3 = mBigPriceGen(3);
-      document.getElementById('bMSlot0').innerHTML = `${mBigItems[3]} for $${bigPrice3}`;
-      document.getElementById('bMSlot0').value = `1~Restaurant~${bigPrice3}`;
-    }
-    lastPick = mBigItems[3];
   }
 }
 
 function mSlotGen() { //function for generating market slot items
   var c = 0;
   var mSlotItems = ['Wood', 'Brick', 'Steel', 'Silver', 'Gold', 'Platinum', 'Cell Phone', 'Computer']; //market slot item
-  var mItemNum = (document.getElementById('mSlots').childNodes.length - 1) / 2; //get how many items
+  //var mItemNum = (document.getElementById('mSlots').childNodes.length - 1) / 2; //get how many items
   numItems = [];
 
-  //-- this the the logic for getting rarity on the items. No they werent picked at random, this is MANY minutes of balancing
-  for (i = 0; i < mItemNum; i++) {
-
+  //this the the logic for getting rarity on the items. No they werent picked at random, this is MANY minutes of balancing
+  for (i = 0; i < mSlotItems.length; i++) {
     var randItem = Math.random() * 100;
 
     if (randItem < 25) { //wood
-      var numItems0 = numOfItem(0);
-      var price0 = mPriceGen(0, numItems0, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems0} ${mSlotItems[0]} for $${price0}`;
-      document.getElementById("mSlot" + i).value = `${numItems0}~Wood~${price0}`;
-    } else if (randItem >= 25 && randItem < 45) { //brick
-      var numItems1 = numOfItem(1);
-      var price1 = mPriceGen(1, numItems1, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems1} ${mSlotItems[1]} for $${price1}`;
-      document.getElementById("mSlot" + i).value = `${numItems1}~Brick~${price1}`;
-    } else if (randItem >= 45 && randItem < 65) { //steel
-      var numItems2 = numOfItem(2);
-      var price2 = mPriceGen(2, numItems2, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems2} ${mSlotItems[2]} for $${price2}`;
-      document.getElementById("mSlot" + i).value = `${numItems2}~Steel~${price2}`;
-    } else if (randItem >= 65 && randItem < 80) { //silver
-      var numItems3 = numOfItem(3);
-      var price3 = mPriceGen(3, numItems3, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems3} ${mSlotItems[3]} for $${price3}`;
-      document.getElementById("mSlot" + i).value = `${numItems3}~Silver~${price3}`;
-    } else if (randItem >= 80 && randItem < 87) { //gold
-      var numItems4 = numOfItem(4);
-      var price4 = mPriceGen(4, numItems4, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems4} ${mSlotItems[4]} for $${price4}`;
-      document.getElementById("mSlot" + i).value = `${numItems4}~Gold~${price4}`;
-    } else if (randItem >= 87 && randItem < 93) { //platinum
-      var numItems5 = numOfItem(5);
-      var price5 = mPriceGen(5, numItems5, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems5} ${mSlotItems[5]} for $${price5}`;
-      document.getElementById("mSlot" + i).value = `${numItems5}~Platinum~${price5}`;
-    } else if (randItem >= 93 && randItem < 97) { //cell phone
-      var numItems6 = numOfItem(6);
-      var price6 = mPriceGen(6, numItems6, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems6} ${mSlotItems[6]} for $${price6}`;
-      document.getElementById("mSlot" + i).value = `${numItems6}~Cell Phone~${price6}`;
-    } else if (randItem >= 97 && randItem <= 100) { //computer
-      var numItems7 = numOfItem(7);
-      var price7 = mPriceGen(7, numItems7, i);
-      document.getElementById("mSlot" + i).innerHTML = `${numItems7} ${mSlotItems[7]} for $${price7}`;
-      document.getElementById("mSlot" + i).value = `${numItems7}~Computer~${price7}`;
+      itemInput(0, 'Wood', i);
+    } else if (randItem < 45) { //brick
+      itemInput(1, 'Brick', i);
+    } else if (randItem < 65) { //steel
+      itemInput(2, 'Steel', i);
+    } else if (randItem < 80) { //silver
+      itemInput(3, 'Silver', i);
+    } else if (randItem < 87) { //gold
+      itemInput(4, 'Gold', i);
+    } else if (randItem < 93) { //platinum
+      itemInput(5, 'Platinum', i);
+    } else if (randItem < 97) { //cell phone
+      itemInput(6, 'Cell Phone', i);
+    } else if (randItem <= 100) { //computer
+      itemInput(7, 'Computer', i);
     }
+  }
+  function itemInput(index, type, i) { //index = number 0-7; type = wood, brick, ect...; i = num for id calls
+    var numItems = numOfItem(index);
+    var price = mPriceGen(index, numItems, i);
+    document.getElementById("mSlot" + i).innerHTML = `${numItems} ${mSlotItems[index]} for $${price}`;
+    document.getElementById("mSlot" + i).value = `${numItems}~${type}~${price}`;
   }
 }
 
-function mPriceGen(a, b, i) { //a is a number 0-7, for the array reference of the product; b is the amount of item; i is for the id call
+function mPriceGen(index, amount, i) { //a is a number 0-7 for the array index of the product; b is the amount of item; i is for the id call
   var mItemPrice = [10, 15, 25, 50, 100, 150, 1000, 1500];
   //(medium +- (medium*.9/rand(+.1))/10 --> round)*number of items
   if (plusMinFn()) {
-    return ((mItemPrice[a] + (mItemPrice[a] * 0.9 / (Math.random() + 0.1)) / 10).toFixed()) * b;
+    return ((mItemPrice[index] + (mItemPrice[index] * 0.9 / (Math.random() + 0.1)) / 10).toFixed()) * amount;
   } else {
-    var rawPrice = (mItemPrice[a] - (mItemPrice[a] * 0.9 / (Math.random() + 0.1)) / 10).toFixed();
+    var rawPrice = (mItemPrice[index] - (mItemPrice[index] * 0.9 / (Math.random() + 0.1)) / 10).toFixed();
 
-    setTimeout(botBuy, (rawPrice / mItemPrice[a] * 10000 - Math.random() * 2), i); //for buying the items
-    return (rawPrice) * b;
+    setTimeout(botBuy, (rawPrice / mItemPrice[index] * 10000 - Math.random() * 2), i); //for buying the items
+    return (rawPrice) * amount;
   }
 }
 
@@ -175,7 +167,7 @@ function mBigPriceGen(a) { //a is a number 0-3
 function numOfItem(a) {
   if (a < 4) {
     return (Math.random() * 9 + 1).toFixed();
-  } else if (a >= 4 && a < 6) {
+  } else if (a < 6) {
     return (Math.random() * 4 + 1).toFixed();
   } else {
     return (Math.random() * 2 + 1).toFixed();
